4448692c27b3cce71422a021ac7ecda7dfc9a237
TwinkleGhodki <160323542+TwinkleGhodki@users.noreply.github.com>
2026-03-26 01:28:26 +0530

    force redeploy

--- backend/server.js
+++ backend/server.js
@@ -22,5 +22,6 @@ app.get('/', (req, res) => {
   res.json({ message: 'BloodConnect API is running' });
 });
 
+console.log("DEPLOY CHECK");
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
